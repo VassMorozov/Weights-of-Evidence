eab9aa802dcc2df8fb84364f7694ab11185664a3
VassMorozov <morozov6@gmail.com>
2024-01-11 11:53:39 +0000

    Update grouping.py
    Added functionality to group additional data based on groupings defined on primary data (df).

--- grouping.py
+++ grouping.py
@@ -16,6 +16,7 @@ class Grouping(object):
             min_samples (int): number of samples in decision tree leaves if grouping_method == 'decision_tree'
             random_state (int): random seed if grouping_method == 'decision_tree'
             n_bins (int): number of bins wanted if grouping_method == 'equal_bins'
+            add_data (pandas.core.frame.DataFrame): additional data to apply groupings calculated on df to
 
         Attributes:
             df (pandas.core.frame.DataFrame): Dataframe containing grouped versions of columns.
@@ -30,9 +31,18 @@ class Grouping(object):
         """
 
     def __init__(self, df, numeric_columns, target, method='decision_tree', depth=2, min_samples=1,
-                 random_state=None, n_bins=2):
+                 random_state=None, n_bins=2, add_data=None):
 
         self.cols_to_group = numeric_columns
+        self.add_data = add_data
+
+        if self.add_data is not None:
+            assert set(self.cols_to_group).issubset(self.add_data.columns), \
+                'Columns to group are not in additional data.'
+            assert (self.add_data.loc[:, self.cols_to_group].isnull().sum() > 0).sum() == 0, \
+                'Missing values found in columns to group for additional data. Deal with these before ' \
+                'calling transformation.'
+            self.add_data = add_data
 
         if method == 'decision_tree':
             self.df = self.decision_tree_grouping(df, target, depth=depth, min_samples=min_samples,
@@ -54,10 +64,17 @@ class Grouping(object):
             # Assign categorical group based on which leaf the value falls into
             df[f"{column}_grouped"] = tree_classifier.apply(df.loc[:, column].values.reshape(-1, 1))
 
+            if self.add_data is not None:
+                self.add_data[f"{column}_grouped"] = tree_classifier.apply(self.add_data.loc[:, column].values.reshape(-1, 1))
+
         return df
 
     def binning(self, df, n_bins):
         for column in self.cols_to_group:
-            df[f'{column}_grouped'] = pd.qcut(df[column], q=n_bins, labels=[i for i in range(n_bins)])
+            df[f'{column}_grouped'], _bins = pd.qcut(df[column], q=n_bins, labels=[i for i in range(n_bins)],
+                                                     retbins=True)
+
+            if self.add_data is not None:
+                self.add_data[f"{column}_grouped"] = pd.cut(self.add_data[column], _bins)
 
         return df
